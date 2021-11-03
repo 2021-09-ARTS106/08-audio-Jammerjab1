ec13c8bbabed7a438effbcaeecaaf7265dcd82e5
Jammerjab1 <89883568+Jammerjab1@users.noreply.github.com>
2021-11-03 12:52:30 -0400

    Stuff from class

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+    public AudioSource audioS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+
+        if(other.CompareTag("EnemyTrigger"))
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+
+
+}

afbce2359ab8163077e2e1507b8ab40c11de4896
Jammerjab1 <89883568+Jammerjab1@users.noreply.github.com>
2021-11-05 20:07:34 -0400

    Finished video

--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -7,6 +7,35 @@ public class PlayerAudio : MonoBehaviour
 {
     public AudioClip splashSound;
     public AudioSource audioS;
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot battleSnapshot;
+    public AudioMixerSnapshot ambIdleSnapshot;
+    public AudioMixerSnapshot ambInSnapshot;
+
+    public LayerMask enemyMask;
+
+    bool enemyNear;
+    private void Update()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 5f, transform.forward, 0f, enemyMask);
+        if(hits.Length > 0)
+        {
+            if (!enemyNear)
+            {
+                battleSnapshot.TransitionTo(0.5f);
+                enemyNear = true;
+            }    
+        }
+        else
+        {
+            if (enemyNear)
+            {
+                idleSnapshot.TransitionTo(0.5f);
+                enemyNear = false;
+            }
+        }
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,8 +46,13 @@ public class PlayerAudio : MonoBehaviour
 
         if(other.CompareTag("EnemyTrigger"))
         {
-
+            battleSnapshot.TransitionTo(0.5f);
         }
+        if (other.CompareTag("Ambience"))
+        {
+            ambInSnapshot.TransitionTo(0.5f);
+        }
+
     }
 
     private void OnTriggerExit(Collider other)
@@ -27,6 +61,16 @@ public class PlayerAudio : MonoBehaviour
         {
             audioS.PlayOneShot(splashSound);
         }
+
+        if (other.CompareTag("EnemyTrigger"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            ambIdleSnapshot.TransitionTo(0.5f);
+        }
+
     }
 
 
